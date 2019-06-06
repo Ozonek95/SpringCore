94d08ea21ef45a0353288af322d4a2af1f94badb
Ozonek95 <kacperstaszek1995@gmail.com>
2019-06-06 11:13:55 +0200

    rozwiązanie zadanie drugie @Import

--- src/main/java/zadanieDrugieImport/DataTransferConfig.java
+++ src/main/java/zadanieDrugieImport/DataTransferConfig.java
@@ -3,6 +3,7 @@ package zadanieDrugieImport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Zwróć uwagę na autowiązanie dataSource.
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Kacper Staszek
  */
 @Configuration
+@Import(DataConfig.class)
 class DataTransferConfig {
   @Autowired
   DataSource<Data> dataSource;
