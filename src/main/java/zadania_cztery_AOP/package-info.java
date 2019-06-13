efbdd465e3be0a7bd0a1c8626640c982b856e770
Ozonek95 <kacperstaszek1995@gmail.com>
2019-06-13 14:52:14 +0200

    Dodano readme i package-info

---
+++ src/main/java/zadania_cztery_AOP/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * W tym pakiecie zapoznasz się z programowaniem aspektowym w Springu.
+ * Poznasz rozdzaje aspektów, dowiesz się jak je definiować i stworzysz własne aspekty.
+ *
+ * @author Kacper Staszek
+ */
+package zadania_cztery_AOP;
---
+++ src/main/java/zadanie_trzy_ApplicationListener/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * W tym pakiecie poznasz zdarzenia w Springu
+ * @see org.springframework.context.ApplicationListener
+ *
+ * @author Kacper Staszek
+ */
+package zadanie_trzy_ApplicationListener;
