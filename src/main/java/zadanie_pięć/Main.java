e843057c1feb8d1eab7817dc0f442f0666b26a79
Ozonek95 <kacperstaszek1995@gmail.com>
2019-06-13 08:41:56 +0200

    Zadanie ApplicationContext vs BeanFactory

---
+++ src/main/java/zadanie_pięć/Main.java
@@ -0,0 +1,21 @@
+package zadanie_pięć;
+
+/**
+ * W tym zadaniu chciałbym zwrócić Twoją uwagę na istotny aspekt.
+ * Czym różni się ApplicationContext od BeanFactory?
+ * Jest to częste pytanie rekrutacyjne i rzecz, którą po prostu warto wiedzieć.
+ *
+ * TODO: Chciałbym, żebyś znalazł odpowiedzi na pytania:
+ *  1.Co jest rozszerzeniem funkcjonalności drugiego?
+ *  2.Jak wygląda inicjalizacja ziaren w jednym i drugim?
+ *  3.Jakie rzeczy są tworzone automatycznie tworząc ApplicationContext, a nie ma ich w BeanFactory?
+ *  4.Co jest zalecane do używania w większości aplikacji, oraz w jakich sytuacjach powinniśmy
+ *  rozważyć użycie drugiego?
+ *
+ *  -https://docs.spring.io/spring/docs/1.2.x/reference/beans.html
+ *
+ * @author Kacper Staszek
+ */
+class Main {
+
+}
